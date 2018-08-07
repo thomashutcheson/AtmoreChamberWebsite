fb9deb5d982f7a3d0aefc5d17385db04e84e12ff
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-07 11:47:21 -0500

    Added content for online store

--- AtmoreChamberPinnacle/Models/IdentityModels.cs
+++ AtmoreChamberPinnacle/Models/IdentityModels.cs
@@ -33,5 +33,7 @@ namespace AtmoreChamberPinnacle.Models
         public System.Data.Entity.DbSet<AtmoreChamberPinnacle.Model.MemberDirectory> MemberDirectories { get; set; }
 
         public System.Data.Entity.DbSet<AtmoreChamberPinnacle.Models.Members> Members { get; set; }
+
+        public System.Data.Entity.DbSet<AtmoreChamberPinnacle.Models.OnlineStore.Product> Products { get; set; }
     }
 }
