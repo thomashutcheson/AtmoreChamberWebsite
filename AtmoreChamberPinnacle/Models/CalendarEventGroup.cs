57c43ebd7c1572c2879cf08eaf90b0a6f538467e
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-08 09:03:46 -0500

    Added Calendar Event group model

---
+++ AtmoreChamberPinnacle/Models/CalendarEventGroup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtmoreChamberPinnacle.Models
+{
+    /// <summary>
+    /// A labeled group of calendar events.
+    /// </summary>
+    /// <remarks>
+    /// This sample groups calendar events by day, so the group title is a formatted date string.
+    /// </remarks>
+    public class CalendarEventGroup
+    {
+        /// <summary>
+        /// Gets or sets a string to show above the group of events.
+        /// </summary>
+        [Required]
+        public string GroupTitle { get; set; }
+        /// <summary>
+        /// Gets or sets a sequence of calendar events to show under the group title.
+        /// </summary>
+        [Required]
+        public IEnumerable<Event> Events { get; set; }
+    }
+}
