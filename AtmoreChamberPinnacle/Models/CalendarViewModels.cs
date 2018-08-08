bf97525f1feff0699531579a986a0d19123078f7
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-08 09:07:45 -0500

    Added CalendarView model

---
+++ AtmoreChamberPinnacle/Models/CalendarViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtmoreChamber.Models
+{
+    /// <summary>
+    /// A view model for the UpcomingEvents view.
+    /// </summary>
+    public class UpcomingEventsViewModel
+    {
+        /// <summary>
+        /// Gets or sets a sequence of event groups to display.
+        /// </summary>
+        [Required]
+        public IEnumerable<CalendarEventGroup> EventGroups { get; set; }
+    }
+}
