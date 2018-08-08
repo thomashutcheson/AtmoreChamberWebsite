945a70783b211f5c9a68c6310544073f960f4057
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-08 09:37:43 -0500

    Added client ID and secret

--- AtmoreChamberPinnacle/App_Start/MyClientSecrets.cs
+++ AtmoreChamberPinnacle/App_Start/MyClientSecrets.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AtmoreChamberPinnacle.App_Start
+﻿namespace AtmoreChamber.App_Start
 {
-    public class MyClientSecrets
+    /// <summary>
+    /// Holds the Google API client secrets. Replace the values below with credentials from your developer console
+    /// (https://console.developers.google.com).
+    /// </summary>
+    internal static class MyClientSecrets
     {
+        public const string ClientId = "441808199947-29l111qlbqvtq2cl68o7nv1ofdf85ntu.apps.googleusercontent.com";
+        public const string ClientSecret = "VRMbCoPPZM-nT_TxTNLXcPAZ";
     }
 }
