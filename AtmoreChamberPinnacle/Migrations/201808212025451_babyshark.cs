f4a04edf39d273771ff95db2a680e2e07e400578
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-21 15:26:18 -0500

    fixed remove item from cart

---
+++ AtmoreChamberPinnacle/Migrations/201808212025451_babyshark.cs
@@ -0,0 +1,16 @@
+namespace AtmoreChamber.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class babyshark : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
