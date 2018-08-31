9b42ac125dfe13d6ce347be5c70415b0c4ccfb3b
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-08-31 10:58:50 -0500

    modified school model

--- AtmoreChamberPinnacle/Models/Jumbotron.cs
+++ AtmoreChamberPinnacle/Models/Jumbotron.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
 namespace AtmoreChamberPinnacle.Models
 {
     public class Jumbotron
     {
+        [Key]
+        public int JumbotronID { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Paragraph")]
+        public string Paragraph { get; set; }
+
+        [DisplayName("Upload Image")]
+        public string ImagePath { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+
     }
 }
--- AtmoreChamberPinnacle/Models/School.cs
+++ AtmoreChamberPinnacle/Models/School.cs
@@ -1,14 +1,24 @@
-﻿namespace AtmoreChamberPinnacle.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtmoreChamberPinnacle.Models
 {
     public class School
     {
 
+        [Key]
         public int SchoolID { get; set; }
 
+        [Required]
+        [DisplayName("Name of School")]
         public string SchoolName { get; set; }
 
-        public string SchoolType { get; set; }
+        [Required]
+        [DisplayName("Type of School")]
+        public string SchoolTypeID { get; set; }
 
+        [Required]
+        [DisplayName("Link to Website")]
         public string SchoolWebsite { get; set; }
 
 
