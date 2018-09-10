220a9b59dd20587ba7acbca8fff85df8af423bb0
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-09-10 13:09:30 -0500

    added xedit and textbox model

---
+++ AtmoreChamberPinnacle/Models/TextBox.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtmoreChamberPinnacle.Models
+{
+    public class TextBox
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+    }
+}
