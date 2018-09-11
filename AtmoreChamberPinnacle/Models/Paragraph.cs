7d327c528b7860c5e000c33295aa9d15c98bf939
John Butterfield <John.ButterfieldProvalus@gmail.com>
2018-09-11 08:28:46 -0500

    added paragraph model

---
+++ AtmoreChamberPinnacle/Models/Paragraph.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtmoreChamberPinnacle.Models
+{
+    public class Paragraph
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+    }
+}
